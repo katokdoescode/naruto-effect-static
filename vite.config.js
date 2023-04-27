3e674cbfddbf3cad4ddedfc6d842df3e1c9c7e98
katokdoescode <snakedoc.md@gmail.com>
2023-04-27 18:45:58 +0400

    Prepare config to build

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,18 @@ import postcssMediaMinmax from 'postcss-media-minmax';
 import cssHasPseudo from 'css-has-pseudo';
 
 export default defineConfig({
-	base: './',
+	base: '/naruto-effect-frontend/',
 	root: './src/',
 	build: {
 		outDir: resolve(__dirname, 'dist/'),
 		emptyOutDir: true,
+		rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        practice: resolve(__dirname, 'src/practice.html'),
+        participant: resolve(__dirname, 'src/participant.html'),
+      },
+    },
 	},
 	plugins: [
 		handlebars({
