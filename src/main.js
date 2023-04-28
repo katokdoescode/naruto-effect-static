2fa93a6620a7badb258d85aaf5eb73c12c360af8
katokdoescode <snakedoc.md@gmail.com>
2023-04-28 17:06:24 +0400

    Improve fallback

--- src/main.js
+++ src/main.js
@@ -3,21 +3,18 @@ if (!CSS.supports('body:has(*)')) {
   const body = document.querySelector('body');
 	const menuBtn = document.querySelector('.menu-btn__btn');
 	const leftSection = document.getElementById('left');
-	leftSection.classList.add('left_has-not-checked');
+	leftSection.setAttribute('menu-is-open', menuBtn.checked);
 
 	menuBtn.addEventListener('input', () => {
 		const isChecked = menuBtn.checked;
 		body.style.overflow = isChecked ? 'hidden' : 'auto';
-
+		leftSection.setAttribute('menu-is-open', menuBtn.checked);
 		const btnWrapper = menuBtn.parentNode;
+
 		if(isChecked) {
-			leftSection.classList.remove('left_has-not-checked');
 			btnWrapper.classList.add('menu-btn_has-checked');
-			leftSection.classList.add('left_has-checked')
 		} else {
-			leftSection.classList.add('left_has-not-checked');
 			btnWrapper.classList.remove('menu-btn_has-checked');
-			leftSection.classList.remove('left_has-checked')
 		}
 	});
 };
