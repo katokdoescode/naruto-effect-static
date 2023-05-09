bb55dce192e52c76839f5c71a392dec4872e00de
katokdoescode <snakedoc.md@gmail.com>
2023-05-09 23:04:00 +0400

    SupportsCSS tests

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
 // :has() fallback function to make menu button works fine
-if (!CSS.supports('body:has(*)')) {
-  const body = document.querySelector('body');
+import * as SupportsCSS from 'supports-css';
+const tests = ['has'];
+SupportsCSS.init({ tests });
+
+if (!SupportsCSSTests.results.has) {
+	const body = document.querySelector('body');
 	const menuBtn = document.querySelector('.menu-btn__btn');
 	const leftSection = document.getElementById('left');
 	leftSection.setAttribute('menu-is-open', menuBtn.checked);
 
 	menuBtn.addEventListener('input', () => {
 		const isChecked = menuBtn.checked;
-		body.style.overflow = isChecked ? 'hidden' : 'auto';
-		leftSection.setAttribute('menu-is-open', menuBtn.checked);
 		const btnWrapper = menuBtn.parentNode;
 
-		if(isChecked) {
-			btnWrapper.classList.add('menu-btn_has-checked');
-			btnWrapper.setAttribute('aria-expanded', isChecked)
-		} else {
+		body.style.overflow = isChecked ? 'hidden' : 'auto';
+		leftSection.setAttribute('menu-is-open', isChecked);
+		btnWrapper.setAttribute('aria-expanded', isChecked)
+
+		isChecked ?
+			btnWrapper.classList.add('menu-btn_has-checked') :
 			btnWrapper.classList.remove('menu-btn_has-checked');
-			btnWrapper.setAttribute('aria-expanded', isChecked)
-		}
 	});
 
 	menuBtn.addEventListener('focusin', () => {
 		const btnWrapper = menuBtn.parentNode;
-		if(menuBtn.matches(':focus-visible'))
+		if (menuBtn.matches(':focus-visible'))
 			btnWrapper.classList.add('menu-btn_has-focus-visible');
 	});
 
